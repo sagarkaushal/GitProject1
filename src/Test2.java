45119711f21c2f1fa35367df467acdaf05ebabce
ionicsagar2909 <ionicsagar2909@gmail.com>
2020-02-06 01:01:56 +0530

    added smthing....

--- src/Test2.java
+++ src/Test2.java
@@ -2,7 +2,7 @@ public class Test2{
 public static void main(String[] args)
 {
 String hungry = "yes";
-if(hungry == "yes")
+if(hungry == "yes" || hungry == "Y" || hungry == "y")
 System.out.println("Eat something....");
 else 
 {
